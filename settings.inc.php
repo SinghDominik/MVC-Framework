7e656f423d1103678425c2ee1558e3743351b3da
SinghDominik <106447873+SinghDominik@users.noreply.github.com>
2022-06-04 16:24:48 +0200

    add settings.inc.php

---
+++ settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+    //general
+    define("PATH", realpath($_SERVER["DOCUMENT_ROOT"]) . "/admin/");
+    define("USE_LANGUAGE", 0);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    session_start();
+
+    //database
+    define("DB_HOST", "");
+    define("DB_USER", "");
+    define("DB_PASSWORD", "");
+    define("DB_NAME", "");
+    define("DB_CHARSET", "");
+?>
